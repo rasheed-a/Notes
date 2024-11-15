0add657d681715926b77ee7eaf072b4681edeb4d
anthonio <anthonio@vostok.cs.widener.edu>
2024-11-15 13:22:45 -0500

    Activity goals

---
+++ src/Activity.java
@@ -0,0 +1,26 @@
+// Exam 3 (Final)
+//	Inheritance & Exceptions
+
+
+// Goal: Have all room descriptions in a File that is read by the Game class.
+/**
+Text File will contain all room descriptions
+
+Room name
+Room description
+# - separates rooms
+repeat
+
+**/
+
+//Game class
+/**
+Add a static HashMap to store room descriptions.(Keys are room names)
+Add a static method that reads text File and populates HashMap.
+**/
+
+//Room class
+/**
+Remove description variable
+Update getDescription() method so it fetches description from Game's HashMap.
+**/
