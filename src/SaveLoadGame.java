2d7af07cf44e72667dbc21a9de5720fa822c8ebf
anthonio <anthonio@vostok.cs.widener.edu>
2024-11-20 13:33:52 -0500

    Save Game Directions*

--- src/SaveLoadGame.java
+++ src/SaveLoadGame.java
@@ -1,4 +1,26 @@
+// 1 - Game class
+/**
+Add static HashMap to hold all Room objects keys are room names.
 
-public class SaveLoadGame {
+Add saveGame Method that stores:
+	currentRoom
+	inventory
+	room map
+to a "save" file
 
-}
+Add loadGame method that loads the currentRoom inventory, and room map from "save" File.
+
+Update switch statement to include "save" and "load"
+**/
+
+//2 - Room class
+/**
+Make class Serializable:
+	**public class Room implements Serializable {**
+In the constructor:
+	Add the room to your HashMap
+**/
+
+//3 - Item class
+/** Make class Serializable **/
+
